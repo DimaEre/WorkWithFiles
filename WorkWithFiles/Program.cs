b436375e988bc553244992eaddb5b48ce3ab83d5
Дмитро Єремія <diameremia2006@gmail.com>
2022-12-07 19:22:17 +0200

    Add project files.

---
+++ WorkWithFiles/Program.cs
@@ -0,0 +1,17 @@
+﻿using System.IO;
+using System;
+
+namespace Program
+{
+    class Програма
+    {
+        static void Main(string[] args)
+        {
+            string? text;
+            text = Console.ReadLine();
+            StreamWriter print = new StreamWriter("File.txt", false);
+            print.Write(text);
+            print.Close();  
+        }
+    }
+}
